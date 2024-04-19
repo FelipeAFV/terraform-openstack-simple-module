23d96d526aa391f0dd7cd6c5b5767b989abcc580
FelipeAFV <ffigueroa@whitestack.com>
2024-04-19 10:38:45 -0400

    first commit

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "openstack_compute_instance_v2" "terrakube_vm" {
   name            = "terrakube_vm"
   image_id        = "8d1c03a5-19f9-4425-8175-864e32c0d74d"
-  flavor_id       = "m1.medium"
+  flavor_id       = "m1.small"
   security_groups = ["default"]
 
   network {
